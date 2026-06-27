28861fb58c69f6dbd7694a8f89d3ee122bc8bd2b
Saakhavat-Prime <sakhavathussayn@gmail.com>
2026-06-27 21:14:33 +0500

    reactDashboard

---
+++ pluseDashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "tailwindcss";
+import PulseDashboard from "./PulseDashboard";
+
+function App() {
+  return (
+    <>
+      <PulseDashboard />
+    </>
+  );
+}
+
+export default App;
